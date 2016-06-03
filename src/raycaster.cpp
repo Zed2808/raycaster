adae86f7268a1618248818ae2dba68478d729f90
Zachary Stevens <zed2808@gmail.com>
2016-06-03 15:58:32 -0400

    moved spritecasting to own function

--- src/raycaster.cpp
+++ src/raycaster.cpp
@@ -223,6 +223,7 @@ void loadWeapons() {
 }
 
 void raycast() {
+	// for each pixel in the width of the screen
 	for(int x = 0; x < w; x++) {
 		// calculate ray position and direction
 		double cameraX = 2 * x / double(w) - 1; // x-coordinate in camera space
@@ -408,6 +409,77 @@ void combSort(int* order, double* dist, int amount) {
 	}
 }
 
+void spritecast() {
+	// sort sprites from furthest to closest
+	for(int i = 0; i < numSprites; i++) {
+		spriteOrder[i] = i;
+		spriteDistance[i] = ((posX - sprite[i].x) * (posX - sprite[i].x) + (posY - sprite[i].y) * (posY - sprite[i].y));
+	}
+
+	combSort(spriteOrder, spriteDistance, numSprites);
+
+	// after sorting the sprites, do the projection and draw them
+	for(int i = 0; i < numSprites; i++) {
+		// translate sprite position to be relative to camera
+		double spriteX = sprite[spriteOrder[i]].x - posX;
+		double spriteY = sprite[spriteOrder[i]].y - posY;
+
+		// transform sprite with the inverse camera matrix:
+		// [ planeX  dirX ] - 1                                            [ dirY     -dirX ]
+		// [              ]        = 1 / (planeX * dirY - dirX * planeY) * [                ]
+		// [ planeY  dirY ]                                                [ -planeY  planeX]
+
+		double invDet = 1.0 / (planeX * dirY - dirX * planeY); // required for correct matrix multiplication
+
+		double transformX = invDet * (dirY * spriteX - dirX * spriteY);
+		double transformY = invDet * (-planeY * spriteX + planeX * spriteY); // depth inside the screen
+
+		int spriteScreenX = int((w / 2) * (1 + transformX / transformY));
+
+		// calculate the height of the sprite on screen
+		int spriteHeight = abs(int(h / transformY)); // using transformY instead of the real distance prevents fisheye
+
+		// calculate lowest and highest pixel to fill in current stripe
+		int drawStartY = -spriteHeight / 2 + h / 2;
+		if(drawStartY < 0) drawStartY = 0;
+
+		int drawEndY = spriteHeight / 2 + h / 2;
+		if(drawEndY >= h) drawEndY = h - 1;
+
+		// calculate the width of the sprite
+		int spriteWidth = abs(int(h / transformY));
+
+		int drawStartX = -spriteWidth / 2 + spriteScreenX;
+		if(drawStartX < 0) drawStartX = 0;
+
+		int drawEndX = spriteWidth / 2 + spriteScreenX;
+		if(drawEndX >= w) drawEndX = w - 1;
+
+		// loop through every vertical stripe of the sprite on screen
+		for(int stripe = drawStartX; stripe < drawEndX; stripe++) {
+			int texX = int(256 * (stripe - (-spriteWidth / 2 + spriteScreenX)) * texWidth / spriteWidth) / 256;
+
+			// the conditions in the if are:
+			// 1) it's in front of the camera plane, so you don't see things behind you
+			// 2) it's on the screen (left)
+			// 3) it's on the screen (right)
+			// 4) ZBuffer, with perpendicular distance
+			if(transformY > 0 && stripe > 0 && stripe < w && transformY < ZBuffer[stripe]) {
+				for(int y = drawStartY; y < drawEndY; y++) {
+					int d = y * 256 - h * 128 + spriteHeight * 128; // 256 and 128 factors to avoid floats
+					int texY = ((d * texHeight) / spriteHeight) / 256;
+					Uint32 color = texture[sprite[spriteOrder[i]].texture][texWidth * texX + texY]; // get current color from the texture
+
+					// color pixel if it isn't the invisible color (black)
+					if((color & 0x00FFFFFF) != 0) {
+						buffer[y][stripe] = color;
+					}
+				}
+			}
+		}
+	}
+}
+
 void drawWeapon(int id, int seq) {
 	// take the sequence in the animation and convert to the actual frame to draw
 	int frame = weapon[id].frameSequence[seq];
@@ -431,6 +503,7 @@ void drawWeapon(int id, int seq) {
 }
 
 int main() {
+	// timing variables
 	unsigned int time = 0;         // time of current frame
 	unsigned int oldTime = 0;      // time of previous frame
 	unsigned int fpsLastShown = 0; // time fps counter was last updated
@@ -451,79 +524,13 @@ int main() {
 	// initialize the window, renderer, and screen texture
 	initScreen(screenWidth, screenHeight, fullscreen, "Raycaster");
 
+	// close if player presses escape
 	while(!done()) {
-		// for each pixel in the width of the screen
+		// draw environment
 		raycast();
 
-		// sprite casting
-		// sort sprites from furthest to closest
-		for(int i = 0; i < numSprites; i++) {
-			spriteOrder[i] = i;
-			spriteDistance[i] = ((posX - sprite[i].x) * (posX - sprite[i].x) + (posY - sprite[i].y) * (posY - sprite[i].y));
-		}
-
-		combSort(spriteOrder, spriteDistance, numSprites);
-
-		// after sorting the sprites, do the projection and draw them
-		for(int i = 0; i < numSprites; i++) {
-			// translate sprite position to be relative to camera
-			double spriteX = sprite[spriteOrder[i]].x - posX;
-			double spriteY = sprite[spriteOrder[i]].y - posY;
-
-			// transform sprite with the inverse camera matrix:
-			// [ planeX  dirX ] - 1                                            [ dirY     -dirX ]
-			// [              ]        = 1 / (planeX * dirY - dirX * planeY) * [                ]
-			// [ planeY  dirY ]                                                [ -planeY  planeX]
-
-			double invDet = 1.0 / (planeX * dirY - dirX * planeY); // required for correct matrix multiplication
-
-			double transformX = invDet * (dirY * spriteX - dirX * spriteY);
-			double transformY = invDet * (-planeY * spriteX + planeX * spriteY); // depth inside the screen
-
-			int spriteScreenX = int((w / 2) * (1 + transformX / transformY));
-
-			// calculate the height of the sprite on screen
-			int spriteHeight = abs(int(h / transformY)); // using transformY instead of the real distance prevents fisheye
-
-			// calculate lowest and highest pixel to fill in current stripe
-			int drawStartY = -spriteHeight / 2 + h / 2;
-			if(drawStartY < 0) drawStartY = 0;
-
-			int drawEndY = spriteHeight / 2 + h / 2;
-			if(drawEndY >= h) drawEndY = h - 1;
-
-			// calculate the width of the sprite
-			int spriteWidth = abs(int(h / transformY));
-
-			int drawStartX = -spriteWidth / 2 + spriteScreenX;
-			if(drawStartX < 0) drawStartX = 0;
-
-			int drawEndX = spriteWidth / 2 + spriteScreenX;
-			if(drawEndX >= w) drawEndX = w - 1;
-
-			// loop through every vertical stripe of the sprite on screen
-			for(int stripe = drawStartX; stripe < drawEndX; stripe++) {
-				int texX = int(256 * (stripe - (-spriteWidth / 2 + spriteScreenX)) * texWidth / spriteWidth) / 256;
-
-				// the conditions in the if are:
-				// 1) it's in front of the camera plane, so you don't see things behind you
-				// 2) it's on the screen (left)
-				// 3) it's on the screen (right)
-				// 4) ZBuffer, with perpendicular distance
-				if(transformY > 0 && stripe > 0 && stripe < w && transformY < ZBuffer[stripe]) {
-					for(int y = drawStartY; y < drawEndY; y++) {
-						int d = y * 256 - h * 128 + spriteHeight * 128; // 256 and 128 factors to avoid floats
-						int texY = ((d * texHeight) / spriteHeight) / 256;
-						Uint32 color = texture[sprite[spriteOrder[i]].texture][texWidth * texX + texY]; // get current color from the texture
-
-						// color pixel if it isn't the invisible color (black)
-						if((color & 0x00FFFFFF) != 0) {
-							buffer[y][stripe] = color;
-						}
-					}
-				}
-			}
-		}
+		// draw sprites
+		spritecast();
 
 		// timing for input and FPS counter
 		oldTime = time;
